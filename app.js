8fe24b3a622626b08b2ff3e78386f2bd38d2f722
Inkagnedo <CCelestin44@gmail.com>
2020-03-01 17:49:11 -0500

    file name/app edits

---
+++ app.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Manager = require("./lib/manager");
+
+const employeeHtml = require("./templates/employee");
+const engineerHtml = require("./templates/engineer");
+const mainHtml = require("./templates/main");
+const managerHtml = require("./templates/manager");
+
+
